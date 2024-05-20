9d5051a590c8394e94746a233f12d0c88af75284
Qiu RA <2271104341@qq.com>
2024-05-20 11:10:25 +0800

    -修改程序

--- XGboostExample/test.py
+++ XGboostExample/test.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 # finalize model and make a prediction for monthly births with xgboost
 from numpy import asarray
 from pandas import read_csv
@@ -15,74 +15,77 @@ from sklearn.metrics import mean_absolute_error
 from xgboost import XGBRegressor
 from matplotlib import pyplot
 
-# transform a time series dataset into a supervised learning dataset
+# 将时间序列数据转换为监督学习数据集
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	n_vars = 1 if type(data) is list else data.shape[1]
 	df = DataFrame(data)
 	cols = list()
-	# input sequence (t-n, ... t-1)
+	# 输入序列 (t-n, ... t-1)
 	for i in range(n_in, 0, -1):
 		cols.append(df.shift(i))
-	# forecast sequence (t, t+1, ... t+n)
+	# 预测序列 (t, t+1, ... t+n)
 	for i in range(0, n_out):
 		cols.append(df.shift(-i))
-	# put it all together
+	# 整合在一起
 	agg = concat(cols, axis=1)
-	# drop rows with NaN values
+	# 删除带有 NaN 值的行
 	if dropnan:
 		agg.dropna(inplace=True)
 	return agg.values
 
-# split a univariate dataset into train/test sets
+# 将单变量数据集拆分为训练集/测试集
 def train_test_split(data, n_test):
 	return data[:-n_test, :], data[-n_test:, :]
 
-# fit an xgboost model and make a one step prediction
+# 拟合 xgboost 模型并进行单步预测
 def xgboost_forecast(train, testX):
-	# transform list into array
+	# 将列表转换为数组
 	train = asarray(train)
-	# split into input and output columns
+	# 拆分为输入和输出列
 	trainX, trainy = train[:, :-1], train[:, -1]
-	# fit model
+	# 拟合模型
 	model = XGBRegressor(objective='reg:squarederror', n_estimators=1000)
 	model.fit(trainX, trainy)
-	# make a one-step prediction
+	# 进行一步预测
 	yhat = model.predict(asarray([testX]))
 	return yhat[0]
 
-# walk-forward validation for univariate data
+# 针对单变量数据的逐步前向验证
 def walk_forward_validation(data, n_test):
 	predictions = list()
-	# split dataset
+	# 拆分数据集
 	train, test = train_test_split(data, n_test)
-	# seed history with training dataset
+	# 使用训练数据集初始化历史数据
 	history = [x for x in train]
-	# step over each time-step in the test set
+	# 遍历测试集中的每个时间步
 	for i in range(len(test)):
-		# split test row into input and output columns
+		# 将测试行拆分为输入和输出列
 		testX, testy = test[i, :-1], test[i, -1]
-		# fit model on history and make a prediction
+		# 在历史数据上拟合模型并进行预测
 		yhat = xgboost_forecast(history, testX)
-		# store forecast in list of predictions
+		# 将预测结果存储在预测列表中
 		predictions.append(yhat)
-		# add actual observation to history for the next loop
+		# 将实际观察结果添加到历史数据中以备下一次循环使用
 		history.append(test[i])
-		# summarize progress
-		print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
-	# estimate prediction error
+		# 汇总进度
+		print('>预期=%.1f, 预测=%.1f' % (testy, yhat))
+	# 估计预测误差
 	error = mean_absolute_error(test[:, -1], predictions)
 	return error, test[:, -1], predictions
 
-# load the dataset
-series = read_csv('./data/daily-total-female-births.csv', header=0, index_col=0)
-values = series.values
-# transform the time series data into supervised learning
+# 加载数据集
+series = read_csv('./data/G_Data0001.csv', header=0, index_col=0)
+print(series.columns)
+series['Err'] = series['Err'].fillna(0)
+series['Alarm'] = series['Alarm'].fillna(0)
+values = series[['GTotal', 'BTotal', 'GFlow', 'BFlow', 'T', 'Pa','Alarm']].values
+# 将时间序列数据转换为监督学习数据
 data = series_to_supervised(values, n_in=6)
-# evaluate
+# 评估
 mae, y, yhat = walk_forward_validation(data, 12)
 print('MAE: %.3f' % mae)
-# plot expected vs preducted
-pyplot.plot(y, label='Expected')
-pyplot.plot(yhat, label='Predicted')
+# 绘制预期 vs 预测图
+pyplot.plot(y, label='预期')
+pyplot.plot(yhat, label='预测')
 pyplot.legend()
 pyplot.show()
